e3160742dbce209a2507a02d3ed51ae1a81012a2
wiktor-jurek <43700411+wiktor-jurek@users.noreply.github.com>
2018-11-08 11:03:01 +0000

    Added stewart number

--- mememachine.c
+++ mememachine.c
@@ -1,1 +1,34 @@
-/************************************************************************** * Assessment Title: ACE 3 * * Number of Submitted C Files: 1 * * Date:  * *  * Author: Wiktor Jurek, Reg no: 201720644 * Author: YOUR_FULL_NAME, Reg no: YOUR_REGISTRATION_NUMBER * Author: YOUR_FULL_NAME, Reg no: YOUR_REGISTRATION_NUMBER * Author: YOUR_FULL_NAME, Reg no: YOUR_REGISTRATION_NUMBER * Author: YOUR_FULL_NAME, Reg no: YOUR_REGISTRATION_NUMBER * * * Personal Statement: We confirm that this submission is all our own work. * *          (Signed)Wiktor Jurek, Stewart Laing, Ryan Green, Maximo Smith, *                  James Chandler * *  * **************************************************************************/ #include <stdio.h>\\And so the fun begins...int main(){    printf("Hello World");    return 0;}
+/**************************************************************************
+ * Assessment Title: ACE 3
+ *
+ * Number of Submitted C Files: 1
+ *
+ * Date: 
+ *
+ * 
+ * Author: Wiktor Jurek, Reg no: 201720644
+ * Author: Stewart Laing, Reg no: 201711046
+ * Author: YOUR_FULL_NAME, Reg no: YOUR_REGISTRATION_NUMBER
+ * Author: YOUR_FULL_NAME, Reg no: YOUR_REGISTRATION_NUMBER
+ * Author: YOUR_FULL_NAME, Reg no: YOUR_REGISTRATION_NUMBER
+ *
+ *
+ * Personal Statement: We confirm that this submission is all our own work.
+ *
+ *          (Signed)Wiktor Jurek, Stewart Laing, Ryan Green, Maximo Smith,
+ *                  James Chandler
+ *
+ * 
+ *
+ **************************************************************************/
+ 
+#include <stdio.h>
+
+\\And so the fun begins...
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
